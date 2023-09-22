07280bdf3b40f72c719ae77c02ff13c14d75f13f
sharkbaby-code <chimaobiezekielamadi@gmail.com>
2023-09-23 01:10:26 +0700

    Add Mouseover Function Black To Grid

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-   createGrid(16);
+   //createGrid(16);
+   let btn = document.querySelector('.new');
+   btn.addEventListener('click', function(){
+   getSize();
+   createGrid();
+});
 } );
 
 function createGrid(size){
@@ -17,6 +22,27 @@ function createGrid(size){
         let cell = document.createElement('div');
         cell.classList.add('grid');
         container.appendChild(cell);
+        cell.addEventListener('mouseover', function(e) {
+        
+            this.setAttribute('style', 'background-color: black;');
+        });
         
     }
+}
+
+
+function getSize() {
+    num = +(prompt("enter number for square grid", ''))
+
+    if (num <= 100) {
+      
+    createGrid(num);
+}
+else if (num.typeOf !== 'number'){
+    alert("please enter a number of divs");
+    
+}
+else {
+    alert("100 is the limit");
+}
 }
